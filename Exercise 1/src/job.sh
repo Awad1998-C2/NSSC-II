dea9ea71da5382230946426aaa6daa155b41522f
Natalia Tylek <nssc_e12332258@tcad30.iue.tuwien.ac.at>
2025-03-30 12:34:37 +0200

    bench

--- Exercise 1/src/job.sh
+++ Exercise 1/src/job.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 ## job name
-
 #SBATCH --job-name=benchmark_1D
 
 ## logfiles (stdout/stderr) %x=job-name %j=job-id
-
 #SBATCH --output=stdout-%x.%j.log
 #SBATCH --error=stderr-%x.%j.log
 
 ## resource requests 
+#SBATCH --partition=nssc         # partition for the cluster
+#SBATCH --nodes=1                # number of nodes
+#SBATCH --ntasks=40              # number of processes
+#SBATCH --cpus-per-task=1        # number of cpus per process
+#SBATCH --time=00:05:00          # set time limit
 
-#SBATCH --partition=nssc    # partition for 360.242 and 360.242
-#SBATCH --nodes=1           # number of nodes
-#SBATCH --ntasks=40         # number of processes
-#SBATCH --cpus-per-task=1   # number of cpus per process
-#SBATCH --time=00:05:00     # set time limit to 1 minute
+## optional: load relevant modules, e.g.:
+# module load mpi/mpich-x86_64 pmi/pmix-x86_64
 
-## load modules and compilation (still on the login node)
-
+## Compilation
 mpic++ -std=c++17 -O3 -Wall -pedantic -march=native -ffast-math main_1D.cpp -o main_1D
 
 resolutions=(125 250 1000 2000)
 iterations=20
 
-# Run benchmarks
+## Run benchmarks
 for res in "${resolutions[@]}"; do
   for nprocs in 1 2 4 8 16 20 32 40; do
     echo "Running resolution=${res}, MPI processes=${nprocs}"
-    mpirun -np ${nprocs} ./solverMPI 1D benchmark_1D_${res}_${nprocs} ${res} ${iterations} 0.0 1.0
+    mpirun -np ${nprocs} ./main_1D 1D benchmark_1D_${res}_${nprocs} ${res} ${iterations} 0.0 1.0
   done
+done
